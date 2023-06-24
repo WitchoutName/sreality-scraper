f7de55366504fa443a28ea1959e5b6923bbd2882
witchoutname <folyn.tomas1@gmail.com>
2023-06-24 08:47:47 +0200

    working beta

---
+++ nexttry/components/Loader.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react"
+
+interface Props {
+  progress: number,
+  goal: number
+}
+
+const Loader:FC<Props> = ({progress, goal}) => {
+  return (
+    <div className="flex justify-center items-center flex-col font-bold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-blue-800 to-indigo-900 p-5">
+      <h1 className="pb-10">The scraper is running...</h1>
+      <div>
+        <b>{progress} </b>/ 
+        <span> {goal}</span>
+      </div>
+    </div>
+  )
+}
+
+export default Loader;
